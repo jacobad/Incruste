39f692c1b988a07aa1ac1e0767de0ac3cd7a0a93
tom <tomperrin.91@outlook.fr>
2019-07-02 09:48:52 +0200

    Ajout de l'écran Filtre et du view controller associé

---
+++ Incruste/SearchFilterVC.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Incruste
+//
+//  Created by Jaco Sanga on 28/06/2019.
+//  Copyright © 2019 Jaco Sanga. All rights reserved.
+//
+
+import UIKit
+
+class SearchFilterVC: UITableViewController {
+
+    @IBOutlet weak var football: UITableViewCell!
+    @IBOutlet weak var basket: UITableViewCell!
+    @IBOutlet weak var tennis: UITableViewCell!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        if indexPath.section == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let cell = tableView.cellForRow(at: indexPath)!
+        if cell.accessoryType == .checkmark {
+            cell.accessoryType = .none
+        } else {
+            cell.accessoryType = .checkmark
+        }
+    }
+}
+
+
+
+
+class Activite {
+    enum Sports {
+        case football (String)
+        case basketball (String)
+        case tennis (String)
+        case sportsdecombat (String)
+        case golf (String)
+        case ski (String)
+    }
+}
